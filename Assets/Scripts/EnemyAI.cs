fd14d610bd2c77f0bc3924225c3ecbb18567fafd
benstew121 <benstew121@gmail.com>
2015-02-19 16:51:19 -0800

    Added Scripts
    Add the player script and started the enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * EnemyAI.cs
+ * 
+ * By Ben Stewart
+ * 
+ * This Scipt is for the Enemies of the game*/
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour 
+{
+
+	[SerializeField]private float patrolSpeed = 2f;
+	[SerializeField]private float patrolWaitTime = 1f;
+	[SerializeField]private Transform [] patrolWayPoints;
+
+	[SerializeField]private NavMeshAgent nav;
+	[SerializeField]private float patrolTimer;
+	[SerializeField]private int wayPointIndex;
+
+
+	private void Awake()
+	{
+		nav = GetComponent<NavMeshAgent> ();
+	}
+
+	private void Update()
+	{
+		Patrolling ();
+	}
+
+	private void Patrolling()
+	{
+		nav.speed = patrolSpeed;
+
+		if ( nav.remainingDistance < nav.stoppingDistance) 
+		{
+						patrolTimer += Time.deltaTime;
+
+
+			if (patrolTimer >= patrolWaitTime) 
+			{
+				if (wayPointIndex == patrolWayPoints.Length - 1)
+					wayPointIndex = 0;
+				else
+				wayPointIndex ++;
+
+
+				patrolTimer = 0;
+
+			}
+
+		} 
+		else
+						
+			patrolTimer = 0;
+
+
+		nav.destination = patrolWayPoints[wayPointIndex].position;
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,172 @@
+﻿/*
+ * Player.cs
+ * by Ben Stewart
+ * 
+ * This makes the player move and able to hit triggers.*/
+
+using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour 
+{
+
+	private CharacterController controller = null;
+
+    public float speed = 10f;
+
+    public float jumpSpeed = 3f;
+
+	public float speedBoost = 5f;
+
+	public GameObject  playerCamera = null;
+
+	private Vector3    velocity     = Vector3.zero;
+
+	private float      rotationY    = 0;
+
+	public float       minimumY     = -60F;
+	public float       maximumY     = 60F;
+
+	public float       sensitivityX = 15F;
+	public float       sensitivityY = 15F;
+
+	public int Score = 0;
+
+	public int maxScore = 15;
+
+	//public GameObject guiUpdater;
+
+	//private GUIUpdater updater;
+	
+
+	// Use this for initialization
+	private void Start () 
+	{
+
+		// references the character controller
+		controller = GetComponent <CharacterController> ();
+		//finds the game object with the GUIUpdater script
+		//guiUpdater = GameObject.Find("ScoreText");
+		// makes it so that you can update the GUIUpdater
+		//updater = guiUpdater.GetComponent<GUIUpdater> ();
+	}
+
+	// Update is called once per frame
+	private void Update () 
+	{
+
+		Screen.showCursor = false;
+		// Assume no input
+		velocity.x = 0;
+		velocity.z = 0;
+
+
+
+		// Get user input
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
+
+
+
+		// Apply all user input to the singular velocity var
+		velocity += transform.right * horizontalInput;
+		velocity += transform.forward * verticalInput;
+
+
+		// you have to be grounded
+		if(controller.isGrounded)
+		{
+            // press space to jump
+	    	if(Input.GetButtonDown("Jump"))               
+		  	{
+			    print ("Jumping");
+
+                velocity.y = jumpSpeed;
+			
+		   	}
+		  else
+			{
+				velocity.y = 0;
+			}
+	    }
+		 
+		velocity += Physics.gravity * Time.deltaTime;
+
+		// Apply all user variable to motion
+		controller.Move ( velocity * speed * Time.deltaTime);
+
+		// Inputs for Mouse
+		float mouseXInput = Input.GetAxis ("Mouse X");
+		float mouseYInput = Input.GetAxis ("Mouse Y");
+
+		this.transform.Rotate (0, mouseXInput * sensitivityX , 0);
+		
+
+
+		rotationY += mouseYInput * sensitivityY;
+		// Gets the value between rotationY, minimumY,and maximumY for rotating the player
+		rotationY = Mathf.Clamp (rotationY,  minimumY, maximumY);
+		
+		playerCamera.transform.localEulerAngles = new Vector3(-rotationY, playerCamera.transform.localEulerAngles.y, 0);
+	}
+
+	/// <summary>
+	/// When you walk into a trigger one of the events will happen
+	/// </summary>
+	/// <param name="other">Other.</param>
+  private void OnTriggerEnter(Collider other)
+    {
+        print("hit");
+		// if you walk into an object that is tagged with Killzone you will die
+        if (other.tag == "Killzone")
+        {
+            Destroy(this.gameObject);
+			Application.LoadLevel("GameOver");
+            print("Dead");
+        }
+
+		// if you walk into an object that is tagged with points will get a point
+		if (other.tag == "Points")
+		{
+			// adds 1 to score
+			Score++;
+			//updates the score text when you get a point
+			//updater.UpdateScore(Score);
+
+		}
+		//if you walk into an object that is tagged with Damage you will lose a point
+		if (other.tag == "Damage") 
+		{
+			// subtracts 1 point from score
+			Score --;
+			// updates the score text when you lose a point
+			//updater.UpdateScore(Score);
+		}
+		// if you walk into WinZone trigger you will go to the win scene
+		if (other.tag == "WinZone")
+		{
+			// if you get max points you will win if you have less you will lose
+			if(Score == maxScore)
+			{
+				print ("Win");
+				Application.LoadLevel("Win");
+			}
+			else
+			{
+				Application.LoadLevel("GameOver");
+			}
+
+		}
+
+		if (other.tag == "SpeedBoost")
+		{
+			speed = speed + speedBoost;
+		}
+    }
+
+
+
+
+}
+
